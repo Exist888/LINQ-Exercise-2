d122d3efe29b22d9a879e797118eca8d3a3e67ca
Filip Herbst <filip_herbst@yahoo.com>
2024-09-03 11:11:41 -0500

    LINQ Exercise 2 - Completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameNamesList = new List<string>(){"Sonic the Hedgehog", "Mario Kart", "Montana '95", "Paperboy"};
+
+            var sortingGameNames = videoGameNamesList.OrderBy(x => x.Length).ToList();
+            
+            Console.WriteLine("Here is a list of old school video games from the '90s, in order of title length:");
+
+            foreach (var game in sortingGameNames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
